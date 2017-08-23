f4a42eada27581013bb52d2d67a75c8ad6419ba4
Andrii <at@winkas.dk>
2017-08-23 12:52:34 +0300

    Init commit

---
+++ src/app/admin/adminShared/user.service.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import {
+    CanActivate,
+    Router,
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot
+} from '@angular/router';
+import * as firebase from 'firebase';
+
+
+@Injectable()
+export class UserService implements CanActivate {
+    userLoggedIn: boolean = false;
+    loggedInUser: string;
+    authUser: any;
+
+    constructor( private router: Router ) {
+        firebase.initializeApp({
+        apiKey: "AIzaSyBMnqBfAOqU1k3bcDdFhhwfLBVVDK6_M_I",
+        authDomain: "webcollection-c7693.firebaseapp.com",
+        databaseURL: "https://webcollection-c7693.firebaseio.com",
+        storageBucket: "webcollection-c7693.appspot.com",
+        messagingSenderId: "785800808578"
+        });
+     }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        let url: string = state.url;
+        return this.verifyLogin(url);
+    }
+
+    verifyLogin(url: string): boolean {
+        if (this.userLoggedIn) { return true; }
+
+        this.router.navigate(['/admin/login']);
+        return false;
+    }
+
+    register(email: string, password: string){
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(function(error) {
+                alert(`${error.message} Please Try Again!`);
+        });
+    }
+
+    verifyUser() {
+        this.authUser = firebase.auth().currentUser;
+        if (this.authUser) {
+
+            alert(`Welcome ${this.authUser.email}`);
+            this.loggedInUser = this.authUser.email;
+            this.userLoggedIn = true;
+            this.router.navigate(['/admin']);
+        }
+    }
+
+    login(loginEmail: string, loginPassword: string) {
+        firebase.auth().signInWithEmailAndPassword(loginEmail, loginPassword)
+            .catch(function(error) {
+                alert(`${error.message} Unable to login. Try again!`);
+        });
+    }
+
+    logout(){
+        this.userLoggedIn = false;
+        firebase.auth().signOut().then(function() {
+            alert(`Logged Out!`);
+
+        }, function(error) {
+            alert(`${error.message} Unable to logout. Try again!`);
+        });
+    }
+
+}
